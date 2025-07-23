27e5e045ca363dde150a5d4375ce135b63776734
ziad elsaadany <ziadelsaadany73@gmail.com>
2025-07-24 00:08:48 +0300

    Open Closed Principle Example 3 Added

---
+++ solid/code/o/open_closed_example3.dart
@@ -0,0 +1,58 @@
+
+
+void main(List<String> args) {
+  final TaxCalculatorTrue taxCalculatorForEgypt = TaxCalculatorTrue(taxCalculatorStrategy: EgyptTaxCalculator());
+  taxCalculatorForEgypt.calculate(1000);
+    final TaxCalculatorTrue taxCalculatorForGermany = TaxCalculatorTrue(
+    taxCalculatorStrategy: GermanyTaxCalculator(),
+  );
+  taxCalculatorForGermany.calculate(1000);
+}
+class TaxCalculator {
+  double calculate(String country, double income) {
+    if (country == 'Egypt') {
+      return income * 0.15;
+    } else if (country == 'Germany') {
+      return income * 0.25;
+    } else if (country == 'USA') {
+      return income * 0.20;
+    }
+    return 0;
+  }
+}
+abstract class TaxCalculatorStrategy {
+  double calculate( double income);
+}
+
+
+class TaxCalculatorTrue {
+
+  final TaxCalculatorStrategy taxCalculatorStrategy;
+
+  TaxCalculatorTrue({required this.taxCalculatorStrategy}); 
+  double calculate( double income) {
+    return taxCalculatorStrategy.calculate(income);
+  }
+}
+
+
+
+class EgyptTaxCalculator implements TaxCalculatorStrategy {
+  @override
+  double calculate(double income) {
+    return income * 0.15;
+  }
+}
+class GermanyTaxCalculator implements TaxCalculatorStrategy {
+  @override
+  double calculate(double income) {
+    return income * 0.25;
+  }
+}
+class USATaxCalculator implements TaxCalculatorStrategy {
+  @override
+  double calculate(double income) {
+    return income * 0.20;
+  }
+}
+
