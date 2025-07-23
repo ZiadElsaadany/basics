d300ef3d49cd0373f51d840288bc16e18324b09c
ziad elsaadany <ziadelsaadany73@gmail.com>
2025-07-24 00:03:52 +0300

    Open Closed Principle Example 2 Added

---
+++ solid/code/o/open_closed_example2.dart
@@ -0,0 +1,47 @@
+// ❌  Violates OCP
+
+
+void main(List<String> args) {
+  // final emailNotifier =PushNotifier(notifierStrategy: EmailNotifier());
+
+  final NotifierStrategy emailNotifier = EmailNotifier(); 
+  emailNotifier.send("Hello");
+  
+}
+class Notifier {
+  void send(String type, String message) {
+    if (type == "email") {
+      print("Sending Email: $message");
+    } else if (type == "sms") {
+      print("Sending SMS: $message");
+    } else if (type == "push") {
+      print("Sending Push Notification: $message");
+    }
+  }
+}
+abstract class NotifierStrategy{
+  void send(String message);
+ }
+ class PushNotifier{  
+
+  final NotifierStrategy notifierStrategy;
+
+  PushNotifier({required this.notifierStrategy});
+
+  void send(String message) {
+    notifierStrategy.send(message);
+  }
+ }
+class EmailNotifier implements NotifierStrategy {
+  @override
+  void send(String message) {
+    print("Sending Email: $message");
+  }
+}
+class SmsNotifier implements NotifierStrategy {
+  @override
+  void send(String message) {
+    print("Sending SMS: $message");
+  }
+}
+
